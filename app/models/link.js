e1f033daa0f153eb73f3fafe29d459d71a768e56
MKS-students <mks.students@hackreactor.com>
2016-06-30 17:13:21 -0500

    first commit

---
+++ app/models/link.js
@@ -0,0 +1,52 @@
+var db = require('../db');
+
+var util = require('../../lib/utility');
+var crypto = require('crypto');
+
+
+var Link = module.exports;
+
+Link.all = function () {
+  return db('links').select('*');
+};
+
+Link.create = function (incomingAttrs) {
+  // Copy to avoid mutation
+  var attrs = Object.assign({}, incomingAttrs);
+
+  var titlePromise = attrs.title
+    ? Promise.resolve(attrs.title)
+    : util.getUrlTitle(attrs.url);
+
+  return titlePromise
+    .then(function (title) {
+
+      attrs.title = title;
+      attrs.visits = 0;
+
+      // Create shortlink token
+      var shasum = crypto.createHash('sha1');
+      shasum.update( attrs.url );
+      attrs.code = shasum.digest('hex').slice(0, 5);
+
+      return db('links').insert(attrs);
+    })
+    .then(function (results) {
+      attrs.id = results.id;
+      return attrs;
+    });
+};
+
+Link.findByUrl = function (url) {
+  return db('links').where({ url: url }).limit(1)
+    .then(function (rows) {
+      return rows[0]
+    });
+};
+
+Link.findByCode = function (code) {
+  return db('links').where({ code: code }).limit(1)
+    .then(function (rows) {
+      return rows[0]
+    });
+};
